3684211f14fc135e0d96b5dc45307fae02a05bce
Evan <evanmlongwood@gmail.com>
2018-09-10 09:40:08 -0700

    Strings and White Space

---
+++ Variables And Simple Data Types/Strings/name.py
@@ -0,0 +1,23 @@
+name = "ada lovelace"
+
+print(name.title())
+
+# Could also use
+print(name.upper())
+print(name.lower())
+
+# Here's another example
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+# Added example
+print("Hello, " + full_name.title() + '!')
+
+# We're going to use Whitespaces
+print("Python")
+print("\tPython")
+
+print("Languages:\n\tPython\n\tC\n\tJavascript")
