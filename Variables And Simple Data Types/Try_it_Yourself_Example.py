16a1d0617252202036f1aec1d3098cde7d7cdc73
Evan <evanmlongwood@gmail.com>
2018-09-10 09:08:52 -0700

    Exercises 2-1 and 2-2

---
+++ Variables And Simple Data Types/Try_it_Yourself_Example.py
@@ -0,0 +1,12 @@
+# 2-1 Simple Message: Store a message in a variable, and then print that message.
+
+message = "Hello to you too!"
+print(message)
+
+# 2-2 Simple messages: Store a message in a variable, and print that message.
+# Then change the value of your variable to a new message and print the new message.
+
+messages = "I don't like pasta"
+print(messages)
+messages = "Nevermind, I do like pasta!"
+print(messages)
