f3887156a7c68346398094cf9e4e14aee29f8bc7
Evan <evanmlongwood@gmail.com>
2018-09-11 14:27:02 -0700

    Try it yourself 3-7.

--- Ch.3 Introducing List/Changing, Adding, and Removing Elements/Try_it_yourself_3_2.py
+++ Ch.3 Introducing List/Changing, Adding, and Removing Elements/Try_it_yourself_3_2.py
@@ -48,3 +48,32 @@ print(dinner_invitations[2] + ", You have been invited to dinner!")
 print(dinner_invitations[3] + ", You have been invited to dinner!")
 print(dinner_invitations[4] + ", You have been invited to dinner!")
 print(dinner_invitations[5] + ", You have been invited to dinner!")
+
+# Shrinking Guest List: You just found out that your new dinner table won't arrive
+# in time for the dinner, and you have space for only two guest.
+# Start with your program from Exercise 3-6. Add a new line that prints a
+# message saying that you can invite only two people for dinner.
+# Use pop() to remove guest from your list one at a time until only two
+# names remain in your list. Each time you pop a name from your list, print a message to that person
+# letting them know you're sorry you can't invite them to dinner.
+# Print a message to each of the two people still on your list, letting them know they're still invited.
+# Use del to remove the last two names from your list, so you have an empty list. Print your list to make
+# sure you actually have an empty list at the end of your program.
+
+print("\nSorry everyone, but our new dinner table won't arrive in time for our event. I only"
+      "have space for two people now.\n")
+cancelled_invitations = dinner_invitations.pop(5)
+print("Sorry, " + cancelled_invitations + " but I can no longer invite you to dinner.")
+cancelled_invitations = dinner_invitations.pop(0)
+print("Sorry, " + cancelled_invitations + " but I can no longer invite you to dinner.")
+cancelled_invitations = dinner_invitations.pop(1)
+print("Sorry, " + cancelled_invitations + " but I can no longer invite you to dinner.")
+cancelled_invitations = dinner_invitations.pop(1)
+print("Sorry, " + cancelled_invitations + " but I can no longer invite you to dinner.")
+
+
+print("\nHey, " + dinner_invitations[0] + " you're still invited! Please come!")
+print("Hey, " + dinner_invitations[1] + " you're still invited! Please come!")
+del dinner_invitations[0]
+del dinner_invitations[0]
+print(dinner_invitations)
