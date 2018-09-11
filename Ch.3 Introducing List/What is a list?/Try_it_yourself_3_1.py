150899f9fdbf66144310fa9248cda7ec84ac4ab6
Evan <evanmlongwood@gmail.com>
2018-09-11 12:34:44 -0700

    Try it yourself 3-1 Exercise.

---
+++ Ch.3 Introducing List/What is a list?/Try_it_yourself_3_1.py
@@ -0,0 +1,10 @@
+# 3-1 Names: Store the names of a few of your friends in a list called names.
+# Print each person's name by accessing each element in the list, one at time.
+
+names = ['frank', 'james', 'johhny', 'sarah', 'samantha']
+
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+print(names[4].title())
