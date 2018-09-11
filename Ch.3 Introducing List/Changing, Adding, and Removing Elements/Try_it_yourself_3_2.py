01a010d6dd949f70d0039fc0ee704cb87534ec5c
Evan <evanmlongwood@gmail.com>
2018-09-11 13:41:39 -0700

    Try it yourself 3-4.

---
+++ Ch.3 Introducing List/Changing, Adding, and Removing Elements/Try_it_yourself_3_2.py
@@ -0,0 +1,10 @@
+# 3-4 Guest List: If you could invite anyone , living or deceased, to dinner, who
+# would you invite? Make a list that includes at least three people you'd like to
+# invite to dinner. Then use your list to print a list to each person, inviting them
+# to dinner.
+
+dinner_invitations = ['Monica', 'Avery', 'Brandon']
+
+print(dinner_invitations[0] + ", You have been invited to dinner!")
+print(dinner_invitations[1] + ", You have been invited to dinner!")
+print(dinner_invitations[2] + ", You have been invited to dinner!")
