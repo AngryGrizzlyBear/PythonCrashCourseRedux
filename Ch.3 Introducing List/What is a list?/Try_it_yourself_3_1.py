049dc79e1d474ae7d3e0ba28d90fb67919f284fc
Evan <evanmlongwood@gmail.com>
2018-09-11 12:40:54 -0700

    Try it yourself 3-2 Exercise.

--- Ch.3 Introducing List/What is a list?/Try_it_yourself_3_1.py
+++ Ch.3 Introducing List/What is a list?/Try_it_yourself_3_1.py
@@ -8,3 +8,13 @@ print(names[1].title())
 print(names[2].title())
 print(names[3].title())
 print(names[4].title())
+
+# 3-2 Start with the list you used in Exercise 3-1, but instead of just print each person's name,
+# print a message to them. The text of each message should be the same ,but each message should be
+# personalized with the person's name.
+
+print("Hello there, " + names[0].title() +  ". Would you like to play a game today?")
+print("What's new, " + names[1].title() + "?")
+print("Want to grab a bite to eat for lunch, " + names[2].title() + "?")
+print("Where are we going today, " + names[3].title() + "?")
+print("Did you and Thomas ever get married, " + names[4].title() + "?" )
