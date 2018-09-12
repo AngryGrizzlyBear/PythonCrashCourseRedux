3159b48d756f26577675e19c0cde023962157b37
Evan <evanmlongwood@gmail.com>
2018-09-12 12:21:57 -0700

    Try it yourself 4-2.

--- Ch.4 Working With List/Looping Through An Entire List./Try_It_Yourself_4_1.py
+++ Ch.4 Working With List/Looping Through An Entire List./Try_It_Yourself_4_1.py
@@ -13,9 +13,32 @@
 pizza_list = ['pepperoni', 'meat lovers', 'sausage']
 for pizza in pizza_list:
     print(pizza)
+
 # Part Two. I wanted to separate these to show the steps of each.
 pizza_list = ['pepperoni', 'meat lovers', 'sausage']
 for pizza in pizza_list:
     print("I really love " + pizza.title() + "!")
 print("I really enjoy all types of pizza. Meat Lovers, Pepperoni, and Sausage are all great pizzas. I "
       "really love pizza!")
+
+# 4-2. Animals: Think of at least three different animals that have a common characteristic.
+# Store the names of these animals in a list, and then use a for loop to
+# print out the name of each animal.
+# •	 Modify your program to print a statement about each animal, such as
+# A dog would make a great pet.
+# •	 Add a line at the end of your program stating what these animals have in
+# common. You could print a sentence such as Any of these animals would
+# make a great pet!
+
+# Part One.
+common_animals = ['dog', 'wolf', 'bear']
+for animal in common_animals:
+    print(animal)
+
+# Part Two. Similar reasons as above.
+common_animals = ['dog', 'wolf', 'bear']
+for animal in common_animals:
+    print("A " + animal + " would make a great pet!")
+print("These animals look very similar and have a lot in common; however, I'm not too sure about"
+      "the bear though.\nYou'll have to do your research on that one. Be careful out there!\nStill"
+      " each has been proven to make great pets!")
