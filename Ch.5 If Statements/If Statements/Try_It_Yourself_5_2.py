0af5f81554f19c837d4f19299f1f904604029ae6
Evan <evanmlongwood@gmail.com>
2018-09-17 14:03:12 -0700

    Example 5-7

--- Ch.5 If Statements/If Statements/Try_It_Yourself_5_2.py
+++ Ch.5 If Statements/If Statements/Try_It_Yourself_5_2.py
@@ -76,3 +76,22 @@ elif stages_of_life < 65:
     print("You're an adult.")
 else:
     print("You're old.")
+
+# 5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series of
+# independent if statements that check for certain fruits in your list.
+# •	 Make a list of your three favorite fruits and call it favorite_fruits.
+# •	 Write five if statements. Each should check whether a certain kind of fruit
+# is in your list. If the fruit is in your list, the if block should print a statement,
+# such as You really like bananas!
+
+favorite_fruits = ['strawberries', 'bananas', 'pineapple', 'cherries', 'mango']
+if 'bananas' in favorite_fruits:
+    print('You really like bananas!')
+if 'strawberries' in favorite_fruits:
+    print('You really like strawberries!')
+if 'cherries' in favorite_fruits:
+    print("You really like cherries!")
+if 'pineapple' in favorite_fruits:
+    print('You really like pineapples!')
+if 'mango' in favorite_fruits:
+    print('You really like mangos!')
