eb570811c52a36862efc2b46960971f50497a2b1
Evan <evanmlongwood@gmail.com>
2018-09-17 13:58:38 -0700

    Example 5-6

--- Ch.5 If Statements/If Statements/Try_It_Yourself_5_2.py
+++ Ch.5 If Statements/If Statements/Try_It_Yourself_5_2.py
@@ -46,3 +46,33 @@ elif 'red' in alien_color:
     print("You just earned 15 points.")
 else:
     print("There aren't any aliens to shoot!")
+
+# 5-6. Stages of Life: Write an if-elif-else chain that determines a person’s
+# stage of life. Set a value for the variable age, and then:
+# •	 If the person is less than 2 years old, print a message that the person is
+# a baby.
+# •	 If the person is at least 2 years old but less than 4, print a message that
+# the person is a toddler.
+# •	 If the person is at least 4 years old but less than 13, print a message that
+# the person is a kid.
+# •	 If the person is at least 13 years old but less than 20, print a message that
+# the person is a teenager.
+# •	 If the person is at least 20 years old but less than 65, print a message that
+# the person is an adult.
+# •	 If the person is age 65 or older, print a message that the person is an
+# elder.
+
+stages_of_life = 70
+
+if stages_of_life < 2:
+    print("You're just a baby.")
+elif stages_of_life < 4:
+    print("You're a toddler.")
+elif stages_of_life < 13:
+    print("You're a kid.")
+elif stages_of_life < 20:
+    print("You're a teenager.")
+elif stages_of_life < 65:
+    print("You're an adult.")
+else:
+    print("You're old.")
