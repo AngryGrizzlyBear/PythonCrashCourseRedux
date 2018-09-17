661264adc42901edeabebf1dbd5a0bc5c25f2e38
Evan <evanmlongwood@gmail.com>
2018-09-17 13:50:48 -0700

    Example 5-5

--- Ch.5 If Statements/If Statements/Try_It_Yourself_5_2.py
+++ Ch.5 If Statements/If Statements/Try_It_Yourself_5_2.py
@@ -27,3 +27,22 @@ if 'green' in alien_color:
     print("You just earned 5 points.")
 else:
     print("You just earned 10 points.")
+
+# 5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elifelse
+# chain.
+# •	 If the alien is green, print a message that the player earned 5 points.
+# •	 If the alien is yellow, print a message that the player earned 10 points.
+# •	 If the alien is red, print a message that the player earned 15 points.
+# •	 Write three versions of this program, making sure each message is printed
+# for the appropriate color alien.
+
+alien_color = ['red']
+
+if 'green' in alien_color:
+    print("You just earned 5 points.")
+elif 'yellow' in alien_color:
+    print("You just earned 10 points.")
+elif 'red' in alien_color:
+    print("You just earned 15 points.")
+else:
+    print("There aren't any aliens to shoot!")
