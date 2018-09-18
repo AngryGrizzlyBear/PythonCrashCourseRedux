665c9a733b02f03d25235ee3db114a3484f07601
Evan <evanmlongwood@gmail.com>
2018-09-18 09:11:32 -0700

    Try it yourself 5-10

--- Ch.5 If Statements/Using If Statements With Lists/Try_It_Yourself_5_4.py
+++ Ch.5 If Statements/Using If Statements With Lists/Try_It_Yourself_5_4.py
@@ -32,3 +32,24 @@ if user_names:
             print("Hello " + user + ", thank you for logging in again!")
 else:
     print("We need to find some users!")
+
+# 5-10. Checking Usernames: Do the following to create a program that simulates
+# how websites ensure that everyone has a unique username.
+# •	 Make a list of five or more usernames called current_users.
+# •	 Make another list of five usernames called new_users. Make sure one or
+# two of the new usernames are also in the current_users list.
+# •	 Loop through the new_users list to see if each new username has already
+# been used. If it has, print a message that the person will need to enter a
+# new username. If a username has not been used, print a message saying
+# that the username is available.
+# •	 Make sure your comparison is case insensitive. If 'John' has been used,
+# 'JOHN' should not be accepted.
+
+current_users = ['jaime', 'thomas', 'martin', 'stephen', 'nancy']
+new_users = ['stacy', 'jaime', 'thomas', 'stan', 'lucy']
+
+for users in new_users:
+    if users in current_users:
+        print("Sorry," + users + " is not available. You will have to input a new username.")
+    else:
+        print(users + ". This name is available!")
