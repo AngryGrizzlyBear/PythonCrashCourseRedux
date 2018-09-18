2d7b6a01719c534fe30319ecf00a96ac66e28019
Evan <evanmlongwood@gmail.com>
2018-09-18 10:15:40 -0700

    Starting empty.

--- Ch.6 Dictionaries/A Simple Dictionary/alien.py
+++ Ch.6 Dictionaries/A Simple Dictionary/alien.py
@@ -2,3 +2,10 @@ alien_0 = {'color': 'green', 'points': 5 }
 
 print(alien_0['color'])
 print(alien_0['points'])
+
+new_points = alien_0['points']
+print("You just earned " + str(new_points) + " points!")
+
+alien_0['x position'] = 0
+alien_0['y position'] = 25
+print(alien_0)
