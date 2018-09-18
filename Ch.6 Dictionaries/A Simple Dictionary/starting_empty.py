a2d50c74dfea4693704663e7f58a7dddfb240c2a
Evan <evanmlongwood@gmail.com>
2018-09-18 10:19:06 -0700

    Modifying Values

--- Ch.6 Dictionaries/A Simple Dictionary/starting_empty.py
+++ Ch.6 Dictionaries/A Simple Dictionary/starting_empty.py
@@ -4,3 +4,10 @@ alien_0 = {}
 alien_0['color'] = 'green'
 alien_0['points'] = 5
 print(alien_0)
+
+# Modifying the values of a dictionary.
+
+alien_0 = {'color': 'green'}
+print("The alien is " + alien_0['color'] + "." )
+alien_0['color'] = 'yellow'
+print("The alien is " + alien_0['color'] + "." )
