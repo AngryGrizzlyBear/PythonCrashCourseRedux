dac7ad118b75ed4a65379bc7698af071da642580
Evan <evanmlongwood@gmail.com>
2018-09-19 14:04:34 -0700

    Chapter 6 Try it yourself.

---
+++ Ch.6 Dictionaries/A Simple Dictionary/Try_it_yourself_6_1.py
@@ -0,0 +1,15 @@
+# 6-1. Person: Use a dictionary to store information about a person you know.
+# Store their first name, last name, age, and the city in which they live. You
+# should have keys such as first_name, last_name, age, and city. Print each
+# piece of information stored in your dictionary.
+
+person = {'first_name': 'Paul',
+          'last_name': 'Stevenson',
+          'age': 34,
+          'city': 'Hekkinville'
+          }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
