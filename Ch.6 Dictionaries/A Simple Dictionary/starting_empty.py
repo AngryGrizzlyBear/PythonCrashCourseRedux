e6c25ca9734b75367d99f2008937ac6d1fe0cd91
Evan <evanmlongwood@gmail.com>
2018-09-19 13:56:47 -0700

    Chapter 5 dictionary practice.

--- Ch.6 Dictionaries/A Simple Dictionary/starting_empty.py
+++ Ch.6 Dictionaries/A Simple Dictionary/starting_empty.py
@@ -8,6 +8,6 @@ print(alien_0)
 # Modifying the values of a dictionary.
 
 alien_0 = {'color': 'green'}
-print("The alien is " + alien_0['color'] + "." )
+print("The alien is " + alien_0['color'] + ".")
 alien_0['color'] = 'yellow'
-print("The alien is " + alien_0['color'] + "." )
+print("The alien is " + alien_0['color'] + ".")
