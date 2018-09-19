297ced3bfa3f66b8a0e1a7034ce26f1a584bb0a3
Evan <evanmlongwood@gmail.com>
2018-09-19 14:08:57 -0700

    Chapter 6 Try it yourself. part 2

--- Ch.6 Dictionaries/A Simple Dictionary/Try_it_yourself_6_1.py
+++ Ch.6 Dictionaries/A Simple Dictionary/Try_it_yourself_6_1.py
@@ -6,10 +6,30 @@
 person = {'first_name': 'Paul',
           'last_name': 'Stevenson',
           'age': 34,
-          'city': 'Hekkinville'
+          'city': 'Hekkinville',
           }
 
 print(person['first_name'])
 print(person['last_name'])
 print(person['age'])
 print(person['city'])
+
+# 6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers.
+# Think of five names, and use them as keys in your dictionary. Think of a favorite
+# number for each person, and store each as a value in your dictionary. Print
+# each person’s name and their favorite number. For even more fun, poll a few
+# friends and get some actual data for your program
+
+favorite_numbers = {
+    'Stan': 32,
+    'Nicole': 7,
+    'Steve': 38,
+    'Mike': 12,
+    'Ross': 343,
+    }
+
+print("Stan's favorite number is " + str(favorite_numbers['Stan']) + "!")
+print("Nicole's favorite number is " + str(favorite_numbers['Nicole']) + "!")
+print("Steve's favorite number is " + str(favorite_numbers['Steve']) + "!")
+print("Mike's favorite number is " + str(favorite_numbers['Mike']) + "!")
+print("Ross's favorite number is " + str(favorite_numbers['Ross']) + "!")
