d49b051d0820f27498a8e0d79a1ba77d77c8d48b
Evan <evanmlongwood@gmail.com>
2018-09-19 14:18:03 -0700

    Chapter 6 Try it yourself. part 3

--- Ch.6 Dictionaries/A Simple Dictionary/Try_it_yourself_6_1.py
+++ Ch.6 Dictionaries/A Simple Dictionary/Try_it_yourself_6_1.py
@@ -33,3 +33,31 @@ print("Nicole's favorite number is " + str(favorite_numbers['Nicole']) + "!")
 print("Steve's favorite number is " + str(favorite_numbers['Steve']) + "!")
 print("Mike's favorite number is " + str(favorite_numbers['Mike']) + "!")
 print("Ross's favorite number is " + str(favorite_numbers['Ross']) + "!")
+
+# 6-3. Glossary: A Python dictionary can be used to model an actual dictionary.
+# However, to avoid confusion, let’s call it a glossary.
+# •	 Think of five programming words you’ve learned about in the previous
+# chapters. Use these words as the keys in your glossary, and store their
+# meanings as values.
+# •	 Print each word and its meaning as neatly formatted output. You might
+# print the word followed by a colon and then its meaning, or print the word
+# on one line and then print its meaning indented on a second line. Use the
+# newline character (\n) to insert a blank line between each word-meaning
+# pair in your output.
+
+programming_meaning = {'Variable': 'To name something.',
+                       'List': 'A variable that holds a collective of information.',
+                       'Dictionary' : 'Holds even more stuff. Think of a bigger list with options.',
+                       'Loops': 'Goes around and around.',
+                       'For Loops': 'Cooler than while loops.'}
+
+word = 'Variable'
+print(word +' : ' + programming_meaning['Variable'])
+word = 'List'
+print("\n" + word + ' : ' + programming_meaning['List'])
+word = 'Dictionary'
+print("\n" + word + ' : ' + programming_meaning['Dictionary'])
+word = 'Loops'
+print("\n" + word + ': ' + programming_meaning['Loops'])
+word = 'For loops'
+print("\n"+ word + ' : ' + programming_meaning['For Loops'])
