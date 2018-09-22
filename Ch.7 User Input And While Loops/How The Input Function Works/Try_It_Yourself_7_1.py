15d315f02129b0a36486c58576c65133afac6fea
Evan Longwood <evanmlongwood@gmail.com>
2018-09-22 08:57:20 -0700

    Try it yourself 7-1

---
+++ Ch.7 User Input And While Loops/How The Input Function Works/Try_It_Yourself_7_1.py
@@ -0,0 +1,7 @@
+# 7-1. Rental Car:
+#  Write a program that asks the user what kind of rental car they
+# would like. Print a message about that car, such as “Let me see if I can find you
+# a Subaru.
+
+car = input("What kind of rental car would you like to rent today? : ")
+print("Okay, let me see if I can find you a " + car + ".")
