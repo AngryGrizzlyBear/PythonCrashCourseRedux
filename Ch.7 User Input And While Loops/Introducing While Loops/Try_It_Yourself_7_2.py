e3cfb2cde5fc3f73b2f063154ecc55700b2a6211
Evan <evanmlongwood@gmail.com>
2018-09-24 08:40:34 -0700

    7-4 and 5.

--- Ch.7 User Input And While Loops/Introducing While Loops/Try_It_Yourself_7_2.py
+++ Ch.7 User Input And While Loops/Introducing While Loops/Try_It_Yourself_7_2.py
@@ -17,11 +17,25 @@ while topping != 'quit':
 # $15. Write a loop in which you ask users their age, and then tell them the cost
 # of their movie ticket.
 
+prompt = "How old are you?"
+prompt += "Enter 'quit when you are finished."
+
+while True:
+    age = input(prompt)
+    if age == 'quit':
+        break
+    age = int(age)
+
+    if age < 3:
+        print(" You get in free!")
+    elif age < 13:
+        print(" Your ticket is $10.")
+    else:
+        print(" Your ticket is $15.")
 # 7-6. Three Exits: Write different versions of either Exercise 7-4 or Exercise 7-5
 # that do each of the following at least once:
 # •	 Use a conditional test in the while statement to stop the loop.
 # •	 Use an active variable to control how long the loop runs.
 # •	 Use a break statement to exit the loop when the user enters a 'quit' value
-
 # 7-7. Infinity: Write a loop that never ends, and run it. (To end the loop, press
 # ctrl-C or close the window displaying the output.)
