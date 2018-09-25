f8253fbd42ce6d6e0507090307c70a9be2d314d5
Evan <evanmlongwood@gmail.com>
2018-09-25 09:29:40 -0700

    Do it yourself 8-7

--- Ch.8 Functions/Return Values/Try_It_Yourself_8_3.py
+++ Ch.8 Functions/Return Values/Try_It_Yourself_8_3.py
@@ -1,6 +1,8 @@
 # 8-6. City Names: Write a function called city_country() that takes in the name
 # of a city and its country. The function should return a string formatted like this:
 # "Santiago, Chile"
+# Call your function with at least three city-country pairs, and print the value
+# that’s returned.
 
 def city_country(city, country):
     """Return a string like 'Santiago, Chilie'."""
@@ -13,8 +15,6 @@ print(city)
 city = city_country(' longyearbeyen', 'svalbard')
 print(city)
 
-# Call your function with at least three city-country pairs, and print the value
-# that’s returned.
 # 8-7. Album: Write a function called make_album() that builds a dictionary
 # describing a music album. The function should take in an artist name and an
 # album title, and it should return a dictionary containing these two pieces of
@@ -25,6 +25,28 @@ print(city)
 # number of tracks on an album. If the calling line includes a value for the number
 # of tracks, add that value to the album’s dictionary. Make at least one new
 # function call that includes the number of tracks on an album.
+def make_album(artist, title, tracks=0):
+    """Build a dictionary containing information about an album."""
+    album_dict = {
+        'artist': artist.title(),
+        'title': title.title(),
+    }
+    if tracks:
+        album_dict['tracks'] = tracks
+    return album_dict
+
+album = make_album('metallica', 'ride the lightning')
+print(album)
+
+album = make_album('beethoven', 'ninth symphony')
+print(album)
+
+album = make_album('willie nelson', 'red-headed stranger')
+print(album)
+
+album = make_album('iron maiden', 'piece of mind', tracks=8)
+print(album)
+
 # 8-8. User Albums: Start with your program from Exercise 8-7. Write a while
 # loop that allows users to enter an album’s artist and title. Once you have that
 # information, call make_album() with the user’s input and print the dictionary
