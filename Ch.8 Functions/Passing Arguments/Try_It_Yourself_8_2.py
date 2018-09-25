690a56bf32b5b2241e3a7983e51379450b5a9222
Evan <evanmlongwood@gmail.com>
2018-09-25 08:07:20 -0700

    Try it yourself 8-4

--- Ch.8 Functions/Passing Arguments/Try_It_Yourself_8_2.py
+++ Ch.8 Functions/Passing Arguments/Try_It_Yourself_8_2.py
@@ -3,15 +3,19 @@
 # a sentence summarizing the size of the shirt and the message printed on it.
 # Call the function once using positional arguments to make a shirt. Call the
 # function a second time using keyword arguments.
-def make_shirt(shirt_size, shirt_message):
+def make_shirt(shirt_size = 'Large', shirt_message = 'I love Python!'):
     print(shirt_size + " is the shirt size.")
     print(shirt_message + " is the shirt message.")
 
-make_shirt('Large', 'I like cheese.')
+make_shirt()
+make_shirt(shirt_size = 'medium')
+make_shirt('small', 'Programmers are loopy.')
 # 8-4. Large Shirts: Modify the make_shirt() function so that shirts are large
 # by default with a message that reads I love Python. Make a large shirt and a
 # medium shirt with the default message, and a shirt of any size with a different
 # message.
+
+
 # 8-5. Cities: Write a function called describe_city() that accepts the name of
 # a city and its country. The function should print a simple sentence, such as
 # Reykjavik is in Iceland. Give the parameter for the country a default value.
