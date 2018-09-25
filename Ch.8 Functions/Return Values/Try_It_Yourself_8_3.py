ffe176ecb7766a4514a311275cb40da29959e12e
Evan <evanmlongwood@gmail.com>
2018-09-25 09:37:38 -0700

    Do it yourself 8-8

--- Ch.8 Functions/Return Values/Try_It_Yourself_8_3.py
+++ Ch.8 Functions/Return Values/Try_It_Yourself_8_3.py
@@ -51,3 +51,4 @@ print(album)
 # loop that allows users to enter an album’s artist and title. Once you have that
 # information, call make_album() with the user’s input and print the dictionary
 # that’s created. Be sure to include a quit value in the while loop
+
