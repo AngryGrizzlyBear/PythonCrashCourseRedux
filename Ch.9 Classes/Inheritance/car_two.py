e75521532617d964b54197e28eb71d243492940b
Evan <evanmlongwood@gmail.com>
2018-09-26 13:53:09 -0700

    Instances as Attributes extended.

--- Ch.9 Classes/Inheritance/car_two.py
+++ Ch.9 Classes/Inheritance/car_two.py
@@ -35,6 +35,17 @@ class Battery():
         """Print a statement describing the battery size."""
         print("This car a " + str(self.battery_size) + "-kWh battery")
 
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
 class ElecticCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
     def __init__(self, make, model, year):
@@ -49,3 +60,4 @@ class ElecticCar(Car):
 my_tesla = ElecticCar('tesla', 'model s', '2016')
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
