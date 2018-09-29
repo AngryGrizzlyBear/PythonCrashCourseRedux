499c849970d8887cdccbf0fd49a38631fe8c69ce
Evan Longwood <evanmlongwood@gmail.com>
2018-09-28 22:25:54 -0700

    Replacing Python with C

--- Ch.10 Files and Exceptions/Reading From a File/Try_it_yourself_10_1.py
+++ Ch.10 Files and Exceptions/Reading From a File/Try_it_yourself_10_1.py
@@ -25,4 +25,17 @@ with open(filename) as f:
 for line in lines:
     print(line.rstrip())
 
+# 10-2. Learning C: You can use the replace() method to replace any word in a string with a different word.
+# Read in each line from the  file you just created, learning_python.txt, and replace the word
+# Python with the name of another language, such as C. Print each modified line to the screen.
+
+filename = 'learning_python.txt'
+
+with open(filename) as f:
+    lines = f.readlines()
+
+for line in lines:
+    # Get rid of newline, then replace Python with C.
+    line = line.rstrip()
+print(line.replace('Python', 'C'))
 
