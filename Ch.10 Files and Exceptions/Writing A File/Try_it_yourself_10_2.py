b364136b164b8e58982f9f27b548b0fe8b39891c
Evan Longwood <evanmlongwood@gmail.com>
2018-09-28 23:14:23 -0700

    Try it yourself 10-4

--- Ch.10 Files and Exceptions/Writing A File/Try_it_yourself_10_2.py
+++ Ch.10 Files and Exceptions/Writing A File/Try_it_yourself_10_2.py
@@ -11,6 +11,17 @@ with open(filename, 'w') as f:
 # visit in a file called guest_book.txt Make sure each entry ap0pears on a new line
 # in the file.
 
+filename = 'guest_book.txt'
+
+print("Enter 'quit' when you are finished.")
+while True:
+    name = input("\nWhat's your name? ")
+    if name == 'quit':
+        break
+    else:
+        with open(filename, 'a') as f:
+            f.write(name + "\n")
+        print("Hi " + name + " you've been added to the guest book.")
 # 10-5 Programming Poll: Write a while lloop that asls people why they like programming.
 # Each time someone enters a reason, add their reason to a file that stores all the
 # responses.
