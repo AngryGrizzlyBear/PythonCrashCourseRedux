3b7b743230cd8202d54a5b439bb3c1b89dacc236
Evan Longwood <evanmlongwood@gmail.com>
2018-09-30 12:23:15 -0700

    Try it yourself 10-7

--- Ch.10 Files and Exceptions/Exceptions/Try_it_yourself_10_3.py
+++ Ch.10 Files and Exceptions/Exceptions/Try_it_yourself_10_3.py
@@ -17,11 +17,35 @@ except ValueError:
 else:
     sum = x + y
     print("The sum of " + str(x) + " and " + str(y) + " is " + str(sum) + ".")
+
 # 10-7. Addition Calculator: Wrap your code from Exercise 10-6 in a while loop so the user
 # can continue entering numbers even if they make a mistake and enter text instead of a number.
 
+print("Enter 'q' at any time to quit.\n")
+
+while True:
+    try:
+        x = input("\nGive me a number: ")
+        if x == 'q':
+            break
+
+        x = int(x)
+
+        y = input("Give me another number: ")
+        if y == 'q':
+            break
+
+        y = int(y)
+
+    except ValueError:
+        print("Sorry, I really needed a number.")
+
+    else:
+        sum = x + y
+        print("The sum of " + str(x) + " and " + str(y) + " is " + str(sum) + ".")
+
 # 10-8. Cats and Dogs: Mkae two files, cats.txt and dogs.txt. Store at least three names of cats
-# in the first file and three nmaes of dogs sin the second file. Write a program that tries to read
+# in the first file and three names of dogs sin the second file. Write a program that tries to read
 # these and print the contents of the files to the screen. Wrap your code in a try-except block to
 # catch the FileNotFound error, and print a friendly message if a file is missing. Move one of the files
 # to a different location on your system, and make sure the code in the xcept block executes properly.
