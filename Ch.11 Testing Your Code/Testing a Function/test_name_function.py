d768be8690bc7d4b90dbb4c94db735f2df90ed3b
Evan Longwood <evanmlongwood@gmail.com>
2018-10-01 12:13:51 -0700

    Adding more test cases.

--- Ch.11 Testing Your Code/Testing a Function/test_name_function.py
+++ Ch.11 Testing Your Code/Testing a Function/test_name_function.py
@@ -8,3 +8,10 @@ class NameTestCase(unittest.TestCase):
         """Do names like 'Janis Joplin' work?"""
         formatted_name = get_formatted_name('janis', 'joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
+
+    def test_first_last_middle_name(self):
+        """Do names like 'Wolfgang Amadeus Mozart' work?"""
+        formatted_name = get_formatted_name(
+            'wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
