177fb263a920f41346ad3ea70134729c0f9d58af
Evan Longwood <evanmlongwood@gmail.com>
2018-10-01 13:19:17 -0700

    Try it yourself 11-2

--- Ch.11 Testing Your Code/Testing a Function/test_cities.py
+++ Ch.11 Testing Your Code/Testing a Function/test_cities.py
@@ -9,3 +9,9 @@ class CitiesTestCase(unittest.TestCase):
         """Does a simple city and country pair work?"""
         santiago_chile = city_country('santiago', 'chile')
         self.assertEqual(santiago_chile, 'Santiago, Chile')
+
+    def test_city_country_population(self):
+        """Can I include a population argument?"""
+        santiago_chile = city_country('santiago', 'chile', population=5000000)
+        self.assertEqual(santiago_chile, 'Santiago, Chile - population 5000000')
+        self.assertEqual(santiago_chile, 'Santiago, Chile - population 5000000')
