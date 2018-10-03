4b19f0b31ab3d520fb29300fcf7feb228678623f
Evan <evanmlongwood@gmail.com>
2018-10-03 12:01:40 -0700

    Addition to class specific tests. More

--- Ch.11 Testing Your Code/Testing A Class/test_survey.py
+++ Ch.11 Testing Your Code/Testing A Class/test_survey.py
@@ -2,22 +2,26 @@ import unittest
 from survey import AnnonymousSurvey
 
 class TestAnonmyousSurvey(unittest.TestCase):
-    """Test for the class AnnoymousSurvey"""
+    """Test for the class AnnonymousSurvey"""
+
+    def setUp(self):
+        """
+        Create a survey and a set of responses for use in all test methods.
+        """
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnnonymousSurvey(question)
+        self.responses = ['English, Spanish, Mandarin']
 
     def test_store_single_responses(self):
         """Test that a single response is stored properly."""
-        question = "What language did you first learn to speak?"
-        my_survey = AnnonymousSurvey(question)
-        my_survey.store_response('English')
-
-        self.assertIn('English', my_survey.responses)
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
 
     def test_store_three_responses(self):
         """Test that three individual responses are stored properly."""
-        question = "What language di you first learn to speak?"
-        my_survey = AnnonymousSurvey(question)
-        responses = ['English', 'Spanish', 'Mandarin']
-        for response in responses:
-            my_survey.store_response(response)
+        for response in self.responses:
+            self.my_survey.store_response(response)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
 
 unittest.main()
