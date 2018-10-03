bebbe6fdf3e2e9d71ad3cb996623ea50e8aa966b
Evan <evanmlongwood@gmail.com>
2018-10-03 11:49:37 -0700

    Addition to class specific tests.

--- Ch.11 Testing Your Code/Testing A Class/test_survey.py
+++ Ch.11 Testing Your Code/Testing A Class/test_survey.py
@@ -7,10 +7,17 @@ class TestAnonmyousSurvey(unittest.TestCase):
     def test_store_single_responses(self):
         """Test that a single response is stored properly."""
         question = "What language did you first learn to speak?"
-        my_survey = AnonymousSurvey(question)
+        my_survey = AnnonymousSurvey(question)
         my_survey.store_response('English')
 
         self.assertIn('English', my_survey.responses)
 
+    def test_store_three_responses(self):
+        """Test that three individual responses are stored properly."""
+        question = "What language di you first learn to speak?"
+        my_survey = AnnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
 
 unittest.main()
