b2ffcf8520569d6a19ba296f866293fc460a2af3
Evan <evanmlongwood@gmail.com>
2018-10-08 13:01:17 -0700

    Created the first row of aliens.

--- Part 2/Alien Invasion/alien_invasion.py
+++ Part 2/Alien Invasion/alien_invasion.py
@@ -16,14 +16,13 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Make a ship.
+    # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
-
-    # Make an alien
-    alien = Alien(ai_settings, screen)
-
-    # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # Set the background color.
     bg_color = (230, 230, 230)
@@ -36,7 +35,7 @@ def run_game():
         ship.update()
         bullets.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
