e3e088abae155ec591312a8c12dda1718e91eea7
Evan <evanmlongwood@gmail.com>
2018-10-08 12:37:04 -0700

    Alien now appears on screen.

--- Part 2/Alien Invasion/alien_invasion.py
+++ Part 2/Alien Invasion/alien_invasion.py
@@ -5,6 +5,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -17,6 +18,9 @@ def run_game():
 
     # Make a ship.
     ship = Ship(ai_settings, screen)
+
+    # Make an alien
+    alien = Alien(ai_settings, screen)
 
     # Make a group to store bullets in.
     bullets = Group()
@@ -32,7 +36,7 @@ def run_game():
         ship.update()
         bullets.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 run_game()
