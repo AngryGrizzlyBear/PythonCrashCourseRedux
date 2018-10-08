fa8ed84dcb60fcea17b48b754f6a133b1dcf4849
Evan <evanmlongwood@gmail.com>
2018-10-08 14:30:22 -0700

    Added more aliens.

--- Part 2/Alien Invasion/alien_invasion.py
+++ Part 2/Alien Invasion/alien_invasion.py
@@ -22,7 +22,7 @@ def run_game():
     aliens = Group()
 
     # Create the fleet of aliens.
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Set the background color.
     bg_color = (230, 230, 230)
@@ -36,7 +36,6 @@ def run_game():
         bullets.update()
         gf.update_bullets(bullets)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
-
 
 run_game()
 
