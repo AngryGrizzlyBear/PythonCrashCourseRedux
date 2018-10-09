3bbaf5bbebaaa91462954f007c4beff29c3f811b
Evan <evanmlongwood@gmail.com>
2018-10-09 13:13:57 -0700

    Refractored bullets.

--- Part 2/Alien Invasion/game_functions.py
+++ Part 2/Alien Invasion/game_functions.py
@@ -73,9 +73,13 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
     # Get rid of bullets that have disappeared.
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
-            bullets.remove(bullets)
-    # Check for any bullets that have hit aliens.
-    # If so, get rid of the bullet and the alien.
+            bullets.remove(bullet)
+
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """Respond to bullet collisions"""
+    # Remove any bullets and aliens that have collided.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
