e5cd414bd608ba6d6b3a0fd65bfd5b79f3e62c23
Evan <evanmlongwood@gmail.com>
2018-10-10 09:50:42 -0700

    Adjusted difficulty that resets after a loss.

--- Part 2/Alien Invasion/game_functions.py
+++ Part 2/Alien Invasion/game_functions.py
@@ -62,6 +62,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
     """Start a new game when the player clicks Play."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Reset the game settings.
+        ai_settings.initialize_dynamic_settings()
         # Hide the mouse cursor.
         pygame.mouse.set_visible(False)
 
@@ -118,8 +120,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
-        # Destroys existing bullets and create a new fleet.
+        # Destroys existing bullets, speed up game, and create a new fleet.
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 def get_number_aliens_x(ai_settings, alien_width):
